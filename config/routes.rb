5ea7b3c5c4c499e8a7a416f5a27279298f5b8cac
Dennis Gabriel <dennisgabr745@gmail.com>
2023-09-09 21:43:46 -0300

    Add User:devise views and style to login User page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :tasks
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
